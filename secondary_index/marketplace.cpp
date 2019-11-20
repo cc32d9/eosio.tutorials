44afeebbdee99bd0a6be23d198648970db51468a
cc32d9 <cc32d9@gmail.com>
2019-11-20 14:53:32 +0000

    updated secondary_index to newest CDT

--- secondary_index/marketplace.cpp
+++ secondary_index/marketplace.cpp
@@ -1,38 +1,38 @@
-#include <eosiolib/eosio.hpp>
-#include <eosiolib/multi_index.hpp>
+#include <eosio/eosio.hpp>
+#include <eosio/multi_index.hpp>
 
 using namespace eosio;
 
-class marketplace : public eosio::contract {
+class [[eosio::contract]] marketplace : public eosio::contract {
 public:
-  marketplace( account_name self ):
-    contract(self), _items(self, self)
-  {}
+  marketplace(name self, name code, datastream<const char*> ds):
+    contract(self, code, ds), _items(self, self.value)
+    {}
 
-  /// @abi table
-  struct item
+  
+  struct [[eosio::table("items")]] item
   {
     uint64_t       id;
-    account_name   owner;
+    name           owner;
     uint64_t       product;
     int64_t        haveqty;
 
     auto primary_key()const { return id; }
-    uint64_t get_owner()const { return owner; }
+    uint64_t get_owner()const { return owner.value; }
     uint64_t get_product()const { return product; }
   };
 
-  typedef eosio::multi_index<N(item), item,
-    indexed_by<N(owner), const_mem_fun<item, uint64_t, &item::get_owner>>,
-    indexed_by<N(product), const_mem_fun<item, uint64_t, &item::get_product>>> items;
+  typedef eosio::multi_index<name("items"), item,
+    indexed_by<name("owner"), const_mem_fun<item, uint64_t, &item::get_owner>>,
+    indexed_by<name("product"), const_mem_fun<item, uint64_t, &item::get_product>>> items;
 
-  /// @abi action
-  void haveprod(account_name owner, uint64_t product, int64_t qty) {
+  [[eosio::action]]
+  void haveprod(name owner, uint64_t product, int64_t qty) {
     _additem(owner, product, qty);
   }
 
-  /// @abi action
-  void wantprod(account_name owner, uint64_t product, int64_t qty) {
+  [[eosio::action]]
+  void wantprod(name owner, uint64_t product, int64_t qty) {
     _additem(owner, product, qty * -1);
   }
 
@@ -41,11 +41,11 @@ public:
 private:
   items _items;
 
-  void _additem(account_name owner, uint64_t product, int64_t haveqty)
+  void _additem(name owner, uint64_t product, int64_t haveqty)
   {
-    require_auth( owner );
+    require_auth(owner);
 
-    auto prodidx = _items.get_index<N(product)>();
+    auto prodidx = _items.get_index<name("product")>();
     auto proditr = prodidx.lower_bound(product);
     while( proditr != prodidx.end() && proditr->product == product ) {
       if( proditr->owner == owner ) {
@@ -75,4 +75,3 @@ private:
   }
 };
   
-EOSIO_ABI( marketplace, (haveprod)(wantprod) )
